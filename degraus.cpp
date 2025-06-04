ad3fb5f82fb943132a1bc9098fbec791a661498b
Graziele Barbosa <graziele.fb12@gmail.com>
2025-06-04 17:23:29 -0300

    Update degraus.cpp

--- degraus.cpp
+++ degraus.cpp
@@ -1,3 +1,15 @@
+/*Essa atividade define uma função recursiva chamada degraus que calcula o número de maneiras de subir uma escada com n degraus,
+onde só é permitido subir 1 ou 3 degraus por vez. A lógica é: para chegar no degrau n, 
+você pode ter vindo do degrau n-1 (subindo 1 degrau) ou do n-3 (subindo 3 degraus), 
+então a função retorna a soma dessas duas possibilidades. A base da recursão é:
+
+Se n == 0, há 1 forma válida (ficar parado no chão).
+
+Se n < 0, há 0 formas válidas (passou do limite).
+
+*/
+
+
 #include <iostream>
 
 int degraus(int n) {
