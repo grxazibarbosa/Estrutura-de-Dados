f586ff23eebe255ac79e2f288864ec359c057001
Graziele Barbosa <graziele.fb12@gmail.com>
2025-06-04 17:35:20 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
-#include "queue.h"
+#include <string>
+#include "Tree.h"
+using namespace std;
 
-int main(){
-    
-    int operacao;
-    std::cin >> operacao;
-
-    Fila fila;
-
-    for(int i = 0; i < operacao; i++){
-        
-        int op;
-        std:: cin>>op;
-
-        if(op== 1){
-            int op2;
-            std::cin >> op2;
-            fila.enfeileirar(op2);
-
-        }
-
-        if(op == 2){
-            fila.remove();
-        }
-
-        if(op == 3){
-            fila.mostrar_um();
-        }
-    }
+int main()
+{
+	string line;
+    getline(cin, line);
+    Tree bt(line);
+    cout << bt.size() << endl;
+	return 0;
 }
